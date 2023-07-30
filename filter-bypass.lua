c62d4fad09bf6991c73832656de8623ade7f17d4
Charlie <131938948+0xFE0F@users.noreply.github.com>
2023-07-30 02:42:52 -0700

    Create filter-bypass.lua

---
+++ filter-bypass.lua
@@ -0,0 +1,19 @@
+local ServerScriptService = game:GetService('ServerScriptService')
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
+local ChatServiceRunner = ServerScriptService:WaitForChild('ChatServiceRunner')
+if not ChatServiceRunner then return warn'no chatservicerunner' end
+
+local ChatService = ChatServiceRunner:WaitForChild('ChatService')
+if not ChatService then return warn'no chatservice' end
+
+ChatService = require(ChatService)
+
+ChatService.InternalApplyRobloxFilterAndTranslate = function(self, _, _, Message, _) return true, false, Message end
+ChatService.InternalApplyRobloxFilterNewAPI = function(self, _, Message, _) return true, false, Message end
+ChatService.InternalApplyRobloxFilter = function(self, _, Message, _) return Message end
+
+for _, Speaker in pairs(ChatService.Speakers) do
+	Speaker:SendSystemMessage('Message filtering has been disabled, enjoy!', 'System')
+	Speaker:SendSystemMessage('https://0xfe0f.github.io/discord', 'System')
+end
